66bbc841b8cf8ca73da7aeb85cc25b04a083b383
megancx <55605051+megancx@users.noreply.github.com>
2021-01-30 20:11:41 -0500

    img

--- types.tsx
+++ types.tsx
@@ -4,14 +4,14 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Record: undefined;
+  Send: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type RecordParamList = {
+  RecordScreen: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+export type SendParamList = {
+  SendScreen: undefined;
 };
